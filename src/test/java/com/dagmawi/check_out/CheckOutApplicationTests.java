c6553349e0e6ce4e0d4a96576f7a40818468fdf6
Dagmawi-22 <dagtt316@gmal.com>
2025-06-07 21:22:32 +0300

    init app with spring initializer

---
+++ src/test/java/com/dagmawi/check_out/CheckOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dagmawi.check_out;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
