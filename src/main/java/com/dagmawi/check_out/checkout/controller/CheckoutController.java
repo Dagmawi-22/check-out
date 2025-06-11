4db0b760af1fec161145249fc250e5010febfa67
Dagmawi <dagtt316@gmail.com>
2025-06-11 15:48:42 +0300

    checkout page with amount validation

--- src/main/java/com/dagmawi/check_out/checkout/controller/CheckoutController.java
+++ src/main/java/com/dagmawi/check_out/checkout/controller/CheckoutController.java
@@ -1,5 +1,27 @@
 package com.dagmawi.check_out.checkout.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.*;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@RestController
+@RequestMapping("/api/v1/payment")
 public class CheckoutController {
-    
+
+    @Autowired
+
+    @GetMapping("/initiate")
+    public ResponseEntity<String> initiatePayment() throws IOException {
+        ClassPathResource htmlFile = new ClassPathResource("html/checkout.html");
+        String html = StreamUtils.copyToString(htmlFile.getInputStream(), StandardCharsets.UTF_8);
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_HTML)
+                .body(html);
+    }
+
 }
