99eb2a72f55b6c7093a28414e8ae7e3fa3bea0e0
Dagmawi <dagtt316@gmail.com>
2025-06-11 14:42:21 +0300

    test db connection

--- src/main/java/com/dagmawi/check_out/service/LivenessService.java
+++ src/main/java/com/dagmawi/check_out/service/LivenessService.java
@@ -1,10 +1,26 @@
 
 package com.dagmawi.check_out.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class LivenessService {
+    
+    @Autowired
+    private DataSource dataSource;
+    
+    public boolean isDatabaseConnected() {
+        try (Connection connection = dataSource.getConnection()) {
+            return !connection.isClosed();
+        } catch (SQLException e) {
+            return false;
+        }
+    }
     private boolean isAlive = true;
 
     public boolean isAlive() {
