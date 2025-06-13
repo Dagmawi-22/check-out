ee90ab1381e38ec9672599f195c72494941f1ecd
Dagmawi <dagtt316@gmail.com>
2025-06-13 15:26:51 +0300

    global instance classes to reuse to log issues

---
+++ email-service/src/main/java/com/daggy/email_service/util/Loggable.java
@@ -0,0 +1,9 @@
+
+package com.dagmawi.email_service.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class Loggable {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+}
